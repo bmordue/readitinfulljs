5a84205a91871557ff6bb95d71ffb4b632f7e537
Ben Mordue <benmordue@gmail.com>
2014-03-07 13:39:25 +0000

    Copypasta from http://mherman.org/blog/2013/11/10/social-authentication-with-passport-dot-js/#.UxnE8FpdVhs

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// client API keys and secrets
+var config = require('./oauth.js')
 
 /**
  * Module dependencies.
@@ -8,11 +10,14 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var mongoose = require('mongoose');
+var passport = require('passport')
+var TwitterStrategy = require('passport-twitter').Strategy;
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 1337);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -34,3 +39,67 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+// serialize and deserialize
+passport.serializeUser(function(user, done) {
+done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+done(null, obj);
+});
+
+// config
+passport.use(new Strategy({
+ clientID: config.facebook.clientID,
+ clientSecret: config.facebook.clientSecret,
+ callbackURL: config.facebook.callbackURL
+},
+function(accessToken, refreshToken, profile, done) {
+ process.nextTick(function () {
+   return done(null, profile);
+ });
+}
+));
+
+
+// TODO: group with other configuration
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session({ secret: 'my_precious' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// routes
+// TOOD: group with other routes
+app.get('/ping', routes.ping);
+app.get('/account', ensureAuthenticated, function(req, res){
+res.render('account', { user: req.user });
+});
+
+app.get('/', function(req, res){
+res.render('login', { user: req.user });
+});
+
+app.get('/auth/twitter',
+passport.authenticate('twitter'),
+function(req, res){
+});
+app.get('/auth/twitter/callback',
+passport.authenticate('twitter', { failureRedirect: '/' }),
+function(req, res) {
+ res.redirect('/account');
+});
+app.get('/logout', function(req, res){
+req.logout();
+res.redirect('/');
+});
+
+// port
+app.listen(1337);
+
+// test authentication
+function ensureAuthenticated(req, res, next) {
+if (req.isAuthenticated()) { return next(); }
+res.redirect('/')
+}
