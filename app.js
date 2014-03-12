1a44afb9b51b994f4774d1c304e49b01fe38433c
Ben Mordue <benmordue@gmail.com>
2014-03-12 22:29:47 +0000

    Fixed it up; make sure app.use(app.router) is after config

--- app.js
+++ app.js
@@ -14,42 +14,6 @@ var mongoose = require('mongoose');
 var passport = require('passport')
 var TwitterStrategy = require('passport-twitter').Strategy;
 
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 1337);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(app.router);
-
-//app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.cookieParser());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.session({ secret: 'my_precious' }));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-//app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
 // serialize and deserialize
 passport.serializeUser(function(user, done) {
 done(null, user);
@@ -65,16 +29,61 @@ passport.use(new TwitterStrategy({
  callbackURL: config.twitter.callbackURL
 },
  function(token, tokenSecret, profile, done) {
-  User.findOrCreate({ twitterId: profile.id }, function (err, user) {
-    return done(err, user);
+     process.nextTick(function() {
+   return done(null, profile);
  });
+
+//  User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+//    return done(err, user);
+// });
 }
 ));
 
 
+var app = express();
+
+// all environments
+app.configure(function () {
+    app.set('port', process.env.PORT || 1337);
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'jade');
+
+    //app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.use(express.methodOverride());
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.cookieParser());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.session({
+        secret: 'my_precious'
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+    
+    app.use(app.router);
+});
+
+app.configure('development',function(){
+// development only
+  app.use(express.errorHandler())
+});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+
 // routes
-// TOOD: group with other routes
+
 //app.get('/ping', routes.ping);
+app.get('/users', user.list);
+
 app.get('/account', ensureAuthenticated, function(req, res){
 res.render('account', { user: req.user });
 });
@@ -84,7 +93,7 @@ res.render('login', { user: req.user });
 });
 
 app.get('/auth/twitter',
-  passport.authenticate('twitter'));
+  passport.authenticate('twitter'), function(req, res){});
 //passport.authenticate('twitter'),
 //function(req, res){
 //});
